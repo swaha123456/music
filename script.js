669666b2b03d1f9a76b8cfe631d856df2605ea66
swaha123456 <swaharoy1@gmail.com>
2025-04-12 11:36:58 +0530

    adme

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("hello")
+
+var Bird = new Audio('img/bird.mp3');
+let Cat = new Audio("assests/cat.mp3");
+let Dog = new Audio("assests/Dog.mp3");
+let Cow = new Audio("assests/goru.mp3");
+let Rain = new Audio("assests/rain.mp3");
+
+function bird() {
+    Bird.play()
+
+}
+
+function birdstop() {
+    Bird.pause()
+
+}
